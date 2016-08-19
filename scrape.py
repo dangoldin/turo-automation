0cd057aecd84bc621401dcadac5fb4c79a716834
Dan Goldin <dangoldin@gmail.com>
2016-08-18 23:30:48 -0400

    Refactored to be a class

--- scrape.py
+++ scrape.py
@@ -9,117 +9,113 @@ RE_REMOVE_HTML = re.compile('<.+?>')
 
 SLEEP_SECONDS = 3
 
-def login(driver):
-    driver.get('https://turo.com/login')
+class TuroCrawler:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.driver.set_page_load_timeout(30)
 
-    username = driver.find_element_by_id('username')
-    username.send_keys(config.TURO_USERNAME)
+    def crawl(self, outfile):
+        self.login()
+        time.sleep(SLEEP_SECONDS)
+        trips = self.get_trips()
+        self.write_to_file(trips, outfile)
 
-    password = driver.find_element_by_name('password')
-    password.send_keys(config.TURO_PASSWORD)
+    def login(self):
+        self.driver.get('https://turo.com/login')
 
-    driver.find_element_by_id("submit").click()
+        username = self.driver.find_element_by_id('username')
+        username.send_keys(config.TURO_USERNAME)
 
-def write_to_file(rows, out):
-    print 'Writing to file', out
-    with open(out, 'w') as f:
-        w = csv.DictWriter(f, delimiter=',', fieldnames=rows[0].keys())
-        w.writeheader()
-        for row in rows:
-            w.writerow(row)
+        password = self.driver.find_element_by_name('password')
+        password.send_keys(config.TURO_PASSWORD)
 
-def get_datetime(el):
-    date = el.find_element_by_class_name('scheduleDate').text
-    time = el.find_element_by_class_name('scheduleTime').text
+        self.driver.find_element_by_id("submit").click()
 
-    date_str = datetime.datetime.now().strftime('%Y') + ' ' + date + ' ' + time
+    def write_to_file(self, rows, out):
+        print 'Writing to file', out
+        with open(out, 'w') as f:
+            w = csv.DictWriter(f, delimiter=',', fieldnames=rows[0].keys())
+            w.writeheader()
+            for row in rows:
+                w.writerow(row)
 
-    return datetime.datetime.strptime(date_str, '%Y %b %d %I:%M %p')
+    def stop():
+        self.driver.close()
 
-def get_trip(driver, reservation_url_snippet):
-    driver.get('https://turo.com' + reservation_url_snippet + '/receipt/')
+    def get_datetime(self, el):
+        date = el.find_element_by_class_name('scheduleDate').text
+        time = el.find_element_by_class_name('scheduleTime').text
 
-    pickup = driver.find_element_by_class_name('reservationSummary-schedulePickUp')
-    dropoff = driver.find_element_by_class_name('reservationSummaryDropOff')
+        date_str = datetime.datetime.now().strftime('%Y') + ' ' + date + ' ' + time
 
-    cost = float(driver.find_element_by_class_name('cost-details').find_element_by_class_name('value').text.replace('$', '').strip())
-    earnings = float(driver.find_element_by_class_name('payment-details').find_element_by_class_name('total').text.replace('$', '').strip())
-    reimbursement_tolls = 0.0
-    reimbursement_mileage = 0.0
+        return datetime.datetime.strptime(date_str, '%Y %b %d %I:%M %p')
 
-    try:
-        reimbursements = driver.find_element_by_class_name('reimbursements').find_elements_by_class_name('line-item--longLabel')
-        for r in reimbursements:
-            if 'tolls' in r.text.lower():
-                reimbursement_tolls = float(r.text.lower().split(' ')[-1])
-            if 'additional miles driven' in r.text.lower():
-                reimbursement_mileage = float(r.text.lower().split(' ')[-1])
-    except Exception, e:
-        print 'No reimbursements found'
+    def get_trip(self, reservation_url_snippet):
+        self.driver.get('https://turo.com' + reservation_url_snippet + '/receipt/')
 
-    return {
-        'url_snippet': reservation_url_snippet,
-        'pickup': get_datetime(pickup),
-        'dropoff': get_datetime(dropoff),
-        'cost': cost,
-        'earnings': earnings,
-        'reimbursement_tolls': reimbursement_tolls,
-        'reimbursement_mileage': reimbursement_mileage,
-    }
+        pickup = self.driver.find_element_by_class_name('reservationSummary-schedulePickUp')
+        dropoff = self.driver.find_element_by_class_name('reservationSummaryDropOff')
 
-# Only trips that have a receipt and have already happened
-def valid_trip(el):
-    return 'completed' in el.get_attribute('class')
+        cost = float(self.driver.find_element_by_class_name('cost-details').find_element_by_class_name('value').text.replace('$', '').strip())
+        earnings = float(self.driver.find_element_by_class_name('payment-details').find_element_by_class_name('total').text.replace('$', '').strip())
+        reimbursement_tolls = 0.0
+        reimbursement_mileage = 0.0
 
-def get_trips(driver, page_slug = None):
-    if page_slug is None:
-        driver.get('https://turo.com/trips')
-    else:
-        print 'Getting https://turo.com/trips?' + page_slug
-        driver.get('https://turo.com/trips?' + page_slug)
+        try:
+            reimbursements = self.driver.find_element_by_class_name('reimbursements').find_elements_by_class_name('line-item--longLabel')
+            for r in reimbursements:
+                if 'tolls' in r.text.lower():
+                    reimbursement_tolls = float(r.text.lower().split(' ')[-1])
+                if 'additional miles driven' in r.text.lower():
+                    reimbursement_mileage = float(r.text.lower().split(' ')[-1])
+        except Exception, e:
+            print 'No reimbursements found for', reservation_url_snippet
 
-    # Get this now and use this later so we don't have to go back
-    next_page = None
-    last_page = driver.find_elements_by_class_name('paginator-link')[-1]
-    if ord(last_page.text) == 8250:
-        next_page = last_page.get_attribute('href').split('?')[-1]
+        return {
+            'url_snippet': reservation_url_snippet,
+            'pickup': self.get_datetime(pickup),
+            'dropoff': self.get_datetime(dropoff),
+            'cost': cost,
+            'earnings': earnings,
+            'reimbursement_tolls': reimbursement_tolls,
+            'reimbursement_mileage': reimbursement_mileage,
+        }
 
-    trip_elements = [te.find_element_by_class_name('reservation') for te in driver.find_elements_by_class_name('reservationSummary') if valid_trip(te)]
+    # Only trips that have a receipt and have already happened
+    def is_valid_trip(self, el):
+        return 'completed' in el.get_attribute('class')
 
-    trip_slugs = [te.get_attribute('data-href') for te in trip_elements]
+    def get_trips(self, page_slug = None):
+        if page_slug is None:
+            self.driver.get('https://turo.com/trips')
+        else:
+            print 'Getting https://turo.com/trips?' + page_slug
+            self.driver.get('https://turo.com/trips?' + page_slug)
 
-    trip_details = [get_trip(driver, trip_slug) for trip_slug in trip_slugs]
+        # Get this now and use this later so we don't have to go back
+        next_page = None
+        last_page = self.driver.find_elements_by_class_name('paginator-link')[-1]
+        if ord(last_page.text) == 8250:
+            next_page = last_page.get_attribute('href').split('?')[-1]
 
-    print trip_details
+        trip_elements = [te.find_element_by_class_name('reservation') for te in self.driver.find_elements_by_class_name('reservationSummary') if self.is_valid_trip(te)]
 
-    # Get the last page link and see if there's more
-    if next_page is not None:
-        trip_details += get_trips(driver, next_page)
+        trip_slugs = [te.get_attribute('data-href') for te in trip_elements]
 
-    return trip_details
+        trip_details = [self.get_trip(trip_slug) for trip_slug in trip_slugs]
 
-def init_driver():
-    driver = webdriver.Chrome()
-    driver.set_page_load_timeout(30)
-    return driver
+        print trip_details
 
-def get_ride_info(outfile):
-    driver = init_driver()
+        # Get the last page link and see if there's more
+        if next_page is not None:
+            trip_details += self.get_trips(next_page)
 
-    login(driver)
-
-    time.sleep(SLEEP_SECONDS)
-
-    trips = get_trips(driver)
-
-    write_to_file(trips, 'out.csv')
-
-    driver.close()
+        return trip_details
 
 if __name__ == '__main__':
     outfile = 'stats.csv'
     if len(sys.argv) > 1:
         outfile = sys.argv[1]
 
-    get_ride_info(outfile)
-
+    crawler = TuroCrawler()
+    crawler.crawl(outfile)
